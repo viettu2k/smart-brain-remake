7155636dfdd1e295583f5a3bec90966cad21469f
viettu2k <doanviettu2k@gmail.com>
2023-09-28 22:04:23 +0700

    adding tsparticles for bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,98 @@
-import './App.css';
+import { useCallback } from 'react';
+import Particles from 'react-tsparticles';
+import { loadSlim } from 'tsparticles-slim';
+import type {
+  Container,
+  Engine,
+  IOptions,
+  RecursivePartial,
+} from 'tsparticles-engine';
+
+const options: RecursivePartial<IOptions> = {
+  fpsLimit: 120,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: 'push',
+      },
+      onHover: {
+        enable: true,
+        mode: 'repulse',
+      },
+      resize: true,
+    },
+    modes: {
+      push: {
+        quantity: 2,
+      },
+      repulse: {
+        distance: 150,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: '#ffffff',
+    },
+    links: {
+      color: '#ffffff',
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    move: {
+      direction: 'none',
+      enable: true,
+      outModes: {
+        default: 'bounce',
+      },
+      random: false,
+      speed: 6,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 120,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: 'circle',
+    },
+    size: {
+      value: { min: 1, max: 5 },
+    },
+  },
+  detectRetina: true,
+};
 
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      await console.log(container);
+    },
+    []
+  );
+
+  return (
+    <Particles
+      id='tsparticles'
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={options}
+    />
+  );
 }
 
 export default App;
