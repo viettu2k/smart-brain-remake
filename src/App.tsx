ab5a3e73887ed017781138fe23e8a8db7d83d740
viettu2k <doanviettu2k@gmail.com>
2023-09-29 23:20:53 +0700

    adding const file

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,9 @@
 import { useCallback } from 'react';
 import Particles from 'react-tsparticles';
 import { loadSlim } from 'tsparticles-slim';
-import type {
-  Container,
-  Engine,
-  IOptions,
-  RecursivePartial,
-} from 'tsparticles-engine';
+import type { Container, Engine } from 'tsparticles-engine';
 
-const options: RecursivePartial<IOptions> = {
-  fpsLimit: 120,
-  interactivity: {
-    events: {
-      onClick: {
-        enable: true,
-        mode: 'push',
-      },
-      onHover: {
-        enable: true,
-        mode: 'repulse',
-      },
-      resize: true,
-    },
-    modes: {
-      push: {
-        quantity: 2,
-      },
-      repulse: {
-        distance: 150,
-        duration: 0.4,
-      },
-    },
-  },
-  particles: {
-    color: {
-      value: '#ffffff',
-    },
-    links: {
-      color: '#ffffff',
-      distance: 150,
-      enable: true,
-      opacity: 0.5,
-      width: 1,
-    },
-    move: {
-      direction: 'none',
-      enable: true,
-      outModes: {
-        default: 'bounce',
-      },
-      random: false,
-      speed: 6,
-      straight: false,
-    },
-    number: {
-      density: {
-        enable: true,
-        area: 800,
-      },
-      value: 120,
-    },
-    opacity: {
-      value: 0.5,
-    },
-    shape: {
-      type: 'circle',
-    },
-    size: {
-      value: { min: 1, max: 5 },
-    },
-  },
-  detectRetina: true,
-};
+import { particlesOptions } from './const';
 
 function App() {
   const particlesInit = useCallback(async (engine: Engine) => {
@@ -90,7 +22,7 @@ function App() {
       id='tsparticles'
       init={particlesInit}
       loaded={particlesLoaded}
-      options={options}
+      options={particlesOptions}
     />
   );
 }
---
+++ src/const/index.ts
@@ -0,0 +1,66 @@
+import { IOptions, RecursivePartial } from 'tsparticles-engine';
+
+export const particlesOptions: RecursivePartial<IOptions> = {
+  fpsLimit: 120,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: 'push',
+      },
+      onHover: {
+        enable: true,
+        mode: 'repulse',
+      },
+      resize: true,
+    },
+    modes: {
+      push: {
+        quantity: 2,
+      },
+      repulse: {
+        distance: 150,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: '#ffffff',
+    },
+    links: {
+      color: '#ffffff',
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    move: {
+      direction: 'none',
+      enable: true,
+      outModes: {
+        default: 'bounce',
+      },
+      random: false,
+      speed: 6,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 120,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: 'circle',
+    },
+    size: {
+      value: { min: 1, max: 5 },
+    },
+  },
+  detectRetina: true,
+};
