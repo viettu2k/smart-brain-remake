4fc3802b79c97a090875407dec067814d3238f64
viettu2k <doanviettu2k@gmail.com>
2023-10-06 22:48:26 +0700

    adding image link form

---
+++ src/components/ImageLinkForm/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const ImageLinkForm: React.FC<{
+  onInputChange: (e: React.ChangeEvent) => void;
+  onButtonSubmit: () => void;
+}> = ({ onInputChange, onButtonSubmit }) => (
+  <div>
+    <p className='text-3xl'>
+      {'This Magic Brain will detect faces in your pictures. Give it a try.'}
+    </p>
+    <div className='flex justify-center bg-image-link-form w-[700px]'>
+      <div className='w-96 p-4 rounded-lg shadow-md'>
+        <input
+          className='w-3/4 px-2 py-1 text-lg rounded-l border border-r-0 focus:outline-none focus:ring focus:border-blue-300'
+          type='text'
+          onChange={onInputChange}
+        />
+        <button
+          className='w-1/4 px-3 py-2 text-lg text-white bg-purple-400 hover:bg-purple-500 rounded-r focus:outline-none focus:ring'
+          onClick={onButtonSubmit}
+        >
+          Detect
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+export default ImageLinkForm;
